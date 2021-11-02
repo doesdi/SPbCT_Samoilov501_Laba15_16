61f3df5cb3da3acefcc4ee25040018217dfddc00
Husokka <samoilovzeka31@mail.ru>
2021-11-02 14:48:42 +0300

    Добавьте файлы проекта.

---
+++ SPbCT_Samoilov501_Laba15_16/SPbCT_Samoilov501_Laba15_16.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, ".1251");
+    float km, ras, cost, sum;
+    cout << "Введите длину пути в км = ";
+    cin >> km;
+    cout << "Введите расход топлива на 100 км = ";
+    cin >> ras;
+    cout << "Введите цену топлива за литр = ";
+    cin >> cost;
+    sum = km * (ras / 100) * cost;
+    cout << "Стоимость поездки = " << sum << " рублей";
+}
